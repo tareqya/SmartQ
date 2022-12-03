da7b77706345b25dab4cd9b9ba31091a9b7660ac
Tareq Yassin <35847896+tareqya@users.noreply.github.com>
2022-12-03 12:48:35 +0200

    add admin firebase to api

--- server/api/functions/index.js
+++ server/api/functions/index.js
@@ -1,4 +1,11 @@
 const functions = require("firebase-functions");
+var admin = require("firebase-admin");
+
+var serviceAccount = require("./serviceAccountKey.json");
+
+admin.initializeApp({
+  credential: admin.credential.cert(serviceAccount),
+});
 
 // // Create and deploy your first functions
 // // https://firebase.google.com/docs/functions/get-started
